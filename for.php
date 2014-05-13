e2c4ef8053b6a45568d5551d8262e6ae777fb20f
Ashley <a_falsario@yahoo.com>
2014-05-13 11:05:53 -0500

    Added error statments to insure a working code despit incorrect input

--- for.php
+++ for.php
@@ -1,8 +1,42 @@
 <?php
 
-define('STARTING', $argv[1]);
-define('ENDING', $argv [2]);
+// if ($argc < 4) {
+// 	echo "Enter in a starting and ending number followed by an increment.\n";
+// 	exit(1);
+// }
 
-for($i = $argv[1]; $i <= $argv[2]; $i++){
+// define('STARTING', $argv[1]);
+// define('ENDING', $argv[2]);
+// define('INCR', $argv[3]);
+
+// for($i = STARTING; $i <= ENDING; $i = $i + INCR){
+// 	echo "$i\n";
+// }
+
+
+fwrite(STDOUT, "Enter in a starting number.\n");
+
+$start = trim(fgets(STDIN));
+
+fwrite(STDOUT, "Thanks! Now enter in an ending number.\n");
+
+$end = trim(fgets(STDIN));
+
+if (!(is_numeric($start)) || (!is_numeric($end))) {
+	echo "ERROR! Please enter in two numeric values.\n";
+	exit(1);
+}
+
+fwrite(STDOUT, "Thanks! Now enter in an increment.\n");
+
+$incr = trim(fgets(STDIN));
+
+if (($incr != TRUE) || !(is_numeric($incr))) {
+	$incr = 1;
+}
+
+
+for($i = $start; $i <= $end; $i += $incr){
 	echo "$i\n";
 }
+
