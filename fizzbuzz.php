b11b471922179c0fd905749a7bf3387eb002b8e1
Ashley <a_falsario@yahoo.com>
2014-05-13 12:54:51 -0500

    fixed formatting

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -3,11 +3,11 @@
 for ($i = 1; $i <= 100; $i++) {
 	if (($i % 3 == 0) && ($i % 5 == 0)) {
 		echo "FizzBuzz\n";	
-		} elseif ($i % 3 == 0) {
+	} elseif ($i % 3 == 0) {
 		echo "Fizz\n";
-		} elseif ($i % 5 == 0) {
+	} elseif ($i % 5 == 0) {
 		echo "Buzz\n";
-		} else {
+	} else {
 		echo "$i\n";
 	}
 }
