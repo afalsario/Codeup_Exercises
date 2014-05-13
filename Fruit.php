ac210a8e07f72e31cb5d458d8318419a25c96f06
Ashley <a_falsario@yahoo.com>
2014-05-13 16:16:00 -0500

    Initial commit of Fruit assignment

---
+++ Fruit.php
@@ -0,0 +1,25 @@
+<?php
+
+// $fruits = ['apple', 'orange', 'banana', 'blueberry'];
+
+// for ($i=0; $i < count($fruits); $i++) { 
+// 	$fruit = $fruits[$i];
+// 	echo $fruit . PHP_EOL;
+// }
+
+// foreach ($fruits as $fruit) {
+// 	echo $fruit . PHP_EOL;
+// }
+
+$fruits = [
+	'apple' 	=> 'red',
+	'orange' 	=> 'orange',
+	'banana' 	=> 'yellow',
+	'blueberry' => 'blue'
+];
+
+foreach($fruits as $fruit => $color) {
+	echo "$fruit is $color" . PHP_EOL;
+}
+
+
