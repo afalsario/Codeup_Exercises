f8409c406498877daa31509b0c673ac425a9f772
Ashley <a_falsario@yahoo.com>
2014-05-13 20:47:11 -0500

    Changed formatting and added a foreach loop

--- foreach_books.php
+++ foreach_books.php
@@ -7,9 +7,9 @@ $books = [
                     'pages'     => 310
     ],
     'Game of Thrones' => [
-                    'published'   => 1996,
-                    'author'      => 'George R. R. Martin',
-                    'pages'       => 835
+                    'published' => 1996,
+                    'author'    => 'George R. R. Martin',
+                    'pages'     => 835
     ],
     'The Catcher in the Rye' => [
                     'published' => 1951,
@@ -17,21 +17,24 @@ $books = [
                     'pages'     => 220
     ],
     'A Tale of Two Cities' => [
-                    'published'        => 1859,
-                    'author'           => 'Charles Dickens',
-                    'pages'            => 544
+                    'published' => 1859,
+                    'author'    => 'Charles Dickens',
+                    'pages'     => 544
     ]
 ];
  
 foreach ($books as $book => $info) {
-//     echo "{$book} was written by {$info['author']} in 
-//         {$info['published']}. It has {$info['pages']} pages." . PHP_EOL;
-// }
+    echo "{$book} was written by {$info['author']} in {$info['published']}. It has {$info['pages']} pages." . PHP_EOL;
+}
 
+echo "\nBooks that were published after 1950:\n";
+
+foreach ($books as $book => $info) {
     if ($info['published'] > 1950) {
         echo $book . PHP_EOL;
     }
 }
+
 
 
 
