6a0df0a2aaafe5fed2e083244811faa4edefa5e6
Ashley <a_falsario@yahoo.com>
2014-05-13 20:45:55 -0500

    Change formatting

--- foreach.php
+++ foreach.php
@@ -2,41 +2,50 @@
 //This is my array
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
-// //This will separate my values in my array
-// foreach ($things as $thing) {
-// //check for integers
-// 	if (is_integer($thing)) {
-// 		echo "{$thing} is an integer\n";
-// 	}
-// //check for floats
-// 	if (is_float($thing)) {
-// 		echo "{$thing} is a float\n";
-// 	}
-// //check for booleans
-// 	if (is_bool($thing)) {
-// 		echo "{$thing} is a boolean\n";
-// 	}
-// //check for arrays
-// 	if (is_array($thing)) {
-// 		echo "the following is an array:\n";
-// 		print_r($thing);
-// 	}
-// //check for null
-// 	if (is_null($thing)) {
-// 		echo "{$thing} is NULL\n";
-// 	}
-// //check for strings
-// 	if (is_string($thing)) {
-// 		echo "{$thing} is a string\n";
-// 	}
-// }
+echo PHP_EOL . "These are the values and their types.\n";
 
-// foreach ($things as $thing) {
-// 	if (is_scalar($thing)) {
-// 		echo $thing . PHP_EOL;
-// 	}
-// }
+//This will separate my values in my array
+foreach ($things as $thing) {
+//check for integers
+	if (is_integer($thing)) {
+		echo "{$thing} is an integer";
+	}
+//check for floats
+	if (is_float($thing)) {
+		echo "{$thing} is a float";
+	}
+//check for booleans
+	if (is_bool($thing)) {
+		echo "{$thing} is a boolean";
+	}
+//check for arrays
+	if (is_array($thing)) {
+		echo "The following is an array:\n";
+		print_r($thing);
+	}
+//check for null
+	if (is_null($thing)) {
+		echo "{$thing} is NULL";
+	}
+//check for strings
+	if (is_string($thing)) {
+		echo "{$thing} is a string";
+	}
+	echo PHP_EOL;
+}
 
+echo PHP_EOL . "These are the scalar values in the array.\n";
+
+//Will output only scalar values
+foreach ($things as $thing) {
+	if (is_scalar($thing)) {
+		echo $thing . PHP_EOL;
+	}
+}
+
+echo PHP_EOL . "These are the types of the values in the array.\n";
+
+//Will only output the type of value
 foreach ($things as $thing) {
 	$type = gettype($thing);
 	echo $type . PHP_EOL;
