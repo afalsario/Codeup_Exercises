aafe8aba65a78c2beb83ebd5086cc9d631754b38
Ashley <a_falsario@yahoo.com>
2014-05-13 20:36:29 -0500

    Fixed formatting

--- Fruit.php
+++ Fruit.php
@@ -1,15 +1,15 @@
 <?php
 
-// $fruits = ['apple', 'orange', 'banana', 'blueberry'];
+$fruits = ['apple', 'orange', 'banana', 'blueberry'];
 
-// for ($i=0; $i < count($fruits); $i++) { 
-// 	$fruit = $fruits[$i];
-// 	echo $fruit . PHP_EOL;
-// }
+for ($i=0; $i < count($fruits); $i++) { 
+	$fruit = $fruits[$i];
+	echo $fruit . PHP_EOL;
+}
 
-// foreach ($fruits as $fruit) {
-// 	echo $fruit . PHP_EOL;
-// }
+foreach ($fruits as $fruit) {
+	echo $fruit . PHP_EOL;
+}
 
 $fruits = [
 	'apple' 	=> 'red',
