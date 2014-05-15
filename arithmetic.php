79856bf839ac0cfcd6af3a8a347b28b8298f205e
Ashley <a_falsario@yahoo.com>
2014-05-15 15:41:28 -0500

    added in check function and removed error checks within other functions

--- arithmetic.php
+++ arithmetic.php
@@ -1,36 +1,35 @@
 <?php
 
-function error($a, $b) {
-		echo "Both $a and $b must be numbers.";
-	}
-
-function add($a, $b) {
+function check($a, $b) {
 	if (is_numeric($a) && is_numeric($b)) {
-    	echo $a + $b;
-    } else {
-    	error($a, $b);
+		return true;
+	} else {
+		echo "Both $a and $b must be numbers.";
+		return false;
+	}
+}
+function add($a, $b) {
+	if (check($a, $b)) {
+    	return $a + $b;
     }
 	echo PHP_EOL;
 }
 
-add(2, 4);
+$sum = add(3, 3);
+echo $sum . PHP_EOL;
 
 function subtract($a, $b) {
-    if (is_numeric($a) && is_numeric($b)) {
+    if (check($a, $b)) {
     	echo $a - $b;
-	} else {
-		error($a, $b);
 	}
 	echo PHP_EOL;
 }
 
-subtract('sdf', 2);
+subtract(4, 2);
 
 function multiply($a, $b) {
-    if (is_numeric($a) && is_numeric($b)) {
+    if (check($a, $b)) {
     	echo $a * $b;
-	} else {
-		error($a, $b);
 	}
 	echo PHP_EOL;
 }
@@ -38,29 +37,26 @@ function multiply($a, $b) {
 multiply(14, 2);
 
 function divide($a, $b) {
-    if (is_numeric($a) && is_numeric($b)) {
+    if (check($a, $b)) {
     	if ($b == 0) {
     		echo "ERROR: You cannot divide by 0.";
     	} else {
     	echo $a / $b;
     	}
-   	} else {
-		error($a, $b);
-	}
+   	}
 	echo PHP_EOL;
 }
 
 divide(14, 0);
 
 function modulus($a, $b) {
-	if (is_numeric($a) && is_numeric($b)) {
+	if (check($a, $b)) {
     	echo $a % $b;
-	} else {
-		error($a, $b);
 	}
 	echo PHP_EOL;
 }
 
 modulus(14, 2);
 
-?>
+
+
