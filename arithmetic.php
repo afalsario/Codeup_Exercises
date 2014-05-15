edf47b39ddb74c837f9b8739c523dcaa40ccc9ad
Ashley <a_falsario@yahoo.com>
2014-05-15 09:48:25 -0500

    called functions

--- arithmetic.php
+++ arithmetic.php
@@ -1,19 +1,27 @@
 <?php
 
 function add($a, $b) {
-    echo $a + $b;
+    echo $a + $b . PHP_EOL;
 }
+
+add(2, 2);
 
 function subtract($a, $b) {
-    echo $a - $b;
+    echo $a - $b . PHP_EOL;
 }
+
+subtract(4, 2);
 
 function multiply($a, $b) {
-    echo $a * $b;
+    echo $a * $b . PHP_EOL;
 }
 
+multiply(2, 2);
+
 function divide($a, $b) {
-    echo $a / $b;
+    echo $a / $b . PHP_EOL;
 }
+
+divide(4, 2);
 
 ?>
