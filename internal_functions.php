78a8919fcf9fcfb8ed2ef7495b9436a49f3b54ba
Ashley <a_falsario@yahoo.com>
2014-05-16 10:57:45 -0500

    created function to test set/empty and serialized

---
+++ internal_functions.php
@@ -0,0 +1,32 @@
+<?php
+
+$nothing = NULL;
+$something = '';
+$array = array(1,2,3);
+
+// Create a function that checks if a variable is set or empty, and display "$variable_name is SET|EMPTY"
+
+function test($variable_name) {
+	if (isset($variable_name)) {
+		echo "variable is SET\n";
+	} elseif (empty($variable_name)) {
+		echo "variable is EMPTY\n";
+	}
+}
+
+test($nothing);
+test($something);
+test($array);
+
+// TEST: If var $nothing is set, display '$nothing is SET'
+
+// TEST: If var $nothing is empty, display '$nothing is EMPTY'
+
+// TEST: If var $something is set, display '$something is SET'
+
+// Serialize the array $array, and output the results
+
+$serial = serialize($array);
+echo $serial . PHP_EOL;
+
+// Unserialize the array $array, and output the results
