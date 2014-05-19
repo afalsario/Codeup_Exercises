dee2b93f07a8d09675b68eeb36c5dba918fb7389
Ashley <a_falsario@yahoo.com>
2014-05-19 14:13:00 -0500

    added function to find values in an array

--- search_arrays.php
+++ search_arrays.php
@@ -4,3 +4,20 @@
 $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
+
+function search($name, $names)
+{
+	if (array_search($name, $names) !== FALSE)
+	{
+		return TRUE;
+	}
+	else
+	{
+		return FALSE;
+	}
+}
+
+$name = 'Tina';
+var_dump(search($name, $names));
+
+
