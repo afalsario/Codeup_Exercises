7cf549b47940c93c324bfb64b0e57e39ab4b647d
Ashley <a_falsario@yahoo.com>
2014-05-20 14:46:59 -0500

    added code to sort array

--- list_with_commas.php
+++ list_with_commas.php
@@ -5,27 +5,25 @@
 $physicists_string = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
 
 $physicists_array = explode(', ', $physicists_string);
+// Humanize that list
+// Converts array into list n1, n2, ..., and n3
+function humanized_list($array) {
+	sort($array);
+	$last = array_pop($array);
+	return implode("\n", $array) . "\nand " . $last;
+}
+
+
+
+echo humanized_list($physicists_array) . PHP_EOL;
 
 $last = array_pop($physicists_array);
-array_push($physicists_array, "and $last");
 
-$famous_fake_physicists = implode(', ', $physicists_array);
+$famous_fake_physicists = implode(", ", $physicists_array) . " and " . $last;
 
 echo "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists}.\n";
 
-// // Converts array into list n1, n2, ..., and n3
-// function humanized_list($array) {
 
-// 	$last = array_pop($array);
-// 	array_push($array, "and $last");
-
-// 	$famous_fake_physicists = implode(', ', $array);
-
-// 	return "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists}\n";
-// }
-
-// // Humanize that list
-// echo humanized_list($physicists_array);
 
 
 
