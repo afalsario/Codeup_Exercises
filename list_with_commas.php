079dc9e4a219e306aaa57fdce32e4b1d9aae8696
Ashley <a_falsario@yahoo.com>
2014-05-20 14:01:05 -0500

    created a function to turn physicists array into a string

--- list_with_commas.php
+++ list_with_commas.php
@@ -13,4 +13,20 @@ $famous_fake_physicists = implode(', ', $physicists_array);
 
 echo "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists}.\n";
 
+// // Converts array into list n1, n2, ..., and n3
+// function humanized_list($array) {
+
+// 	$last = array_pop($array);
+// 	array_push($array, "and $last");
+
+// 	$famous_fake_physicists = implode(', ', $array);
+
+// 	return "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists}\n";
+// }
+
+// // Humanize that list
+// echo humanized_list($physicists_array);
+
+
+
 
