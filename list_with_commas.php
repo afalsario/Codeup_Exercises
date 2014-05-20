7b5afd76443bc78bb01135ae19d1cc4a8ac04bc8
Ashley <a_falsario@yahoo.com>
2014-05-20 15:03:18 -0500

    reformatted function and added option to sort values

--- list_with_commas.php
+++ list_with_commas.php
@@ -7,15 +7,20 @@ $physicists_string = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mal
 $physicists_array = explode(', ', $physicists_string);
 // Humanize that list
 // Converts array into list n1, n2, ..., and n3
-function humanized_list($array) {
-	sort($array);
+function humanized_list($array, $sort = FALSE) {
+	if ($sort)
+	{
+		sort($array);
+	}
 	$last = array_pop($array);
-	return implode("\n", $array) . "\nand " . $last;
+	array_push($array, "and $last");
+	$string = implode(", ", $array);
+	return $string;
 }
 
 
 
-echo humanized_list($physicists_array) . PHP_EOL;
+echo humanized_list($physicists_array, true) . PHP_EOL;
 
 $last = array_pop($physicists_array);
 
